bf1df1d4c4725018e6e039fd6d557ee0053b8b33
ToeiKanta <41673034+ToeiKanta@users.noreply.github.com>
2019-01-20 15:55:40 +0700

    First Version

---
+++ ImageRotation/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ImageRotation
+{
+    partial class ImageRotation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTN_choose_folder = new System.Windows.Forms.Button();
+            this.BTN_Save = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BTN_choose_folder
+            // 
+            this.BTN_choose_folder.Location = new System.Drawing.Point(21, 12);
+            this.BTN_choose_folder.Name = "BTN_choose_folder";
+            this.BTN_choose_folder.Size = new System.Drawing.Size(202, 72);
+            this.BTN_choose_folder.TabIndex = 4;
+            this.BTN_choose_folder.Text = "Choose folder";
+            this.BTN_choose_folder.UseVisualStyleBackColor = true;
+            this.BTN_choose_folder.Click += new System.EventHandler(this.BTN_choose_folder_Click);
+            // 
+            // BTN_Save
+            // 
+            this.BTN_Save.Location = new System.Drawing.Point(21, 90);
+            this.BTN_Save.Name = "BTN_Save";
+            this.BTN_Save.Size = new System.Drawing.Size(202, 60);
+            this.BTN_Save.TabIndex = 5;
+            this.BTN_Save.Text = "Save";
+            this.BTN_Save.UseVisualStyleBackColor = true;
+            this.BTN_Save.Click += new System.EventHandler(this.BTN_Save_Click);
+            // 
+            // ImageRotation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 173);
+            this.Controls.Add(this.BTN_Save);
+            this.Controls.Add(this.BTN_choose_folder);
+            this.Name = "ImageRotation";
+            this.Text = "ImageRotation";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BTN_choose_folder;
+        private System.Windows.Forms.Button BTN_Save;
+    }
+}
+
---
+++ ImageRotation/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ImageRotation
+{
+    public partial class ImageRotation : Form
+    {
+        string filename;
+        string foldername;
+
+        public ImageRotation()
+        {
+            InitializeComponent();
+        }
+
+        private Image imageLoad(string filepath)
+        {
+            string path = $"{foldername}\\{filepath}";
+            try
+            {
+                this.filename = path;
+                Image img = Image.FromFile(path);
+                return img;
+            }
+            catch
+            {
+                //MessageBox.Show(path + " Not Find");
+                return null;
+            }
+            
+        }
+
+        private void BTN_choose_folder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            fbd.Description = "";
+            fbd.ShowNewFolderButton = false;
+
+            if(fbd.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("กดปุ่ม save ได้เลย");
+                this.foldername = fbd.SelectedPath;
+            }
+
+        }
+
+        private void BTN_Save_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("รอแปปนะ จนกว่าจะมีข้อความขึ้นว่าเสร็จแล้ว");
+            DirectoryInfo d = new DirectoryInfo(this.foldername);//Assuming Test is your Folder
+            FileInfo[] Files = d.GetFiles("*.jpg"); //Getting Text files
+            foreach (FileInfo file in Files)
+            {
+                //MessageBox.Show(file.Name);
+                Image File = imageLoad(file.Name);
+
+                string filenameTemp = this.filename.Remove(this.filename.Length - 4);
+
+                File.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                File.Save(filenameTemp + "-1.jpg");
+
+                File.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                File.Save(filenameTemp + "-2.jpg");
+
+                File.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                File.Save(filenameTemp + "-3.jpg");
+            }
+            MessageBox.Show("เสร็จแล้วโว้ยยย...");
+        }
+    }
+}
